081fa446ef0217cde756acfeb1ce27fb0ad1a2e2
lxxbluesea <lxx8368@163.com>
2015-03-18 00:43:34 +0800

    增加了下拉ListView时，更新数据的功能； 修改了ViewPager加载多个Activity时出现的BUG 增加了ListView的Head

--- src/main/java/com/example/donotforgetme/Activities/Normal.java
+++ src/main/java/com/example/donotforgetme/Activities/Normal.java
@@ -2,12 +2,16 @@ package com.example.donotforgetme.Activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.ImageView;
 import android.widget.ListView;
@@ -15,6 +19,7 @@ import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
 import com.example.donotforgetme.Entities.Item;
+import com.example.donotforgetme.Entities.Status;
 import com.example.donotforgetme.MyListener.MyClickTouchListener;
 import com.example.donotforgetme.MyListener.MyPageAdapterListener;
 import com.example.donotforgetme.MyListener.MyPageChangeListener;
@@ -39,7 +44,7 @@ public class Normal extends Fragment {
     //Normal_Executing窗口的代码
     ListView listView;
     ItemUtil itemUtil;
-
+    int type = 1;//分类，传入不同的值，得到不同的结果
     //=========结束==========
 
 
@@ -47,7 +52,7 @@ public class Normal extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         //return super.onCreateView(inflater, container, savedInstanceState);
         System.out.println("onCreateView");
-        return inflater.inflate(R.layout.activity_normal,container,false);
+        return inflater.inflate(R.layout.activity_normal, container, false);
     }
 
     @Override
@@ -59,79 +64,92 @@ public class Normal extends Fragment {
         initViewPager();
     }
 
-    void initViewPager()
-    {
-        viewPager=(ViewPager)getView().findViewById(R.id.normal_viewpager);
-        cursor=(ImageView)getView().findViewById(R.id.normal_cursor);
-        if(viewPager!=null)
-        {
-            views=new ArrayList<View>();
-            titles=new ArrayList<String>();
+    void initViewPager() {
+        viewPager = (ViewPager) getView().findViewById(R.id.normal_viewpager);
+        cursor = (ImageView) getView().findViewById(R.id.normal_cursor);
+        if (viewPager != null) {
+            views = new ArrayList<View>();
+            titles = new ArrayList<String>();
 
-            LayoutInflater layoutInflater=LayoutInflater.from(this.getActivity());
+            LayoutInflater layoutInflater = LayoutInflater.from(this.getActivity());
 
-            views.add(layoutInflater.inflate(R.layout.activity_normal_executing,null));
-            views.add(layoutInflater.inflate(R.layout.activity_normal_delay,null));
-            views.add(layoutInflater.inflate(R.layout.activity_normal_delete,null));
-            views.add(layoutInflater.inflate(R.layout.activity_normal_finish,null));
+            head = layoutInflater.inflate(R.layout.listview_head, null);
+
+            views.add(layoutInflater.inflate(R.layout.activity_normal_executing, null));
+            views.add(layoutInflater.inflate(R.layout.activity_normal_delay, null));
+            views.add(layoutInflater.inflate(R.layout.activity_normal_delete, null));
+            views.add(layoutInflater.inflate(R.layout.activity_normal_finish, null));
+            views.add(layoutInflater.inflate(R.layout.activity_normal_note, null));
 
             titles.add(getResources().getString(R.string.title_activity_executing));
             titles.add(getResources().getString(R.string.title_activity_delay));
             titles.add(getResources().getString(R.string.title_activity_delete));
             titles.add(getResources().getString(R.string.title_activity_finish));
+            titles.add(getResources().getString(R.string.title_activity_note));
 
-            MyPagerAdapter myPagerAdapter=new MyPagerAdapter(views,titles);
+            MyPagerAdapter myPagerAdapter = new MyPagerAdapter(views, titles);
             myPagerAdapter.setMyPageAdapterListener(new MyPageAdapterListener() {
                 @Override
                 public void Execute(int index) {
-                    switch (index)
-                    {
-                        case 0:
-                            initExecuteControls();
-                            break;
-                        case 1:
+                    //如果当前页面和正在加载的页面相同时，则执行加载数据
+                    //因为ViewPager每次划动时，可能加载2-3个页面
+                    if (viewPager.getCurrentItem() == index) {
 
-                            break;
-
-                        case 2:
-
-                            break;
-                        case 3:
-
-                            break;
-                        case 4:
-
-                            break;
-
+                        switch (index) {
+                            case 0:
+                                listView = (ListView) getActivity().findViewById(R.id.normal_execute_listview);
+                                type = StatusUtil.EXECUTE;
+                                break;
+                            case 1:
+                                listView = (ListView) getActivity().findViewById(R.id.normal_delay_listview);
+                                type = StatusUtil.RELAY;
+                                break;
+                            case 2:
+                                listView = (ListView) getActivity().findViewById(R.id.normal_delete_listview);
+                                type = StatusUtil.DELETE;
+                                break;
+                            case 3:
+                                listView = (ListView) getActivity().findViewById(R.id.normal_finish_listview);
+                                type = StatusUtil.FINISH;
+                                break;
+                            case 4:
+                                listView = (ListView) getActivity().findViewById(R.id.normal_note_listview);
+                                type = StatusUtil.NOTE;
+                                break;
+                        }
+                        //获取数据
+                        getDate(type);
                     }
-
                 }
             });
 
             viewPager.setAdapter(myPagerAdapter);
             viewPager.setCurrentItem(0);
-            viewPager.setOnPageChangeListener(new MyPageChangeListener(initTextView(),cursor,getDisplayMetrice(),4));
+            viewPager.setOnPageChangeListener(new MyPageChangeListener(initTextView(), cursor, getDisplayMetrice(), 5));
 
+            itemUtil = new ItemUtil();
         }
     }
 
-    List<TextView> initTextView()
-    {
-        TextView execute,delay,delete,finish;
-        execute=(TextView)getView().findViewById(R.id.normal_execute_tab_title);
-        execute.setOnTouchListener(new MyClickTouchListener(0,viewPager));
-        delay=(TextView)getView().findViewById(R.id.normal_delay_tab_title);
-        delay.setOnTouchListener(new MyClickTouchListener(1,viewPager));
-        delete=(TextView)getView().findViewById(R.id.normal_delete_tab_title);
-        delete.setOnTouchListener(new MyClickTouchListener(2,viewPager));
-        finish=(TextView)getView().findViewById(R.id.normal_finish_tab_title);
-        finish.setOnTouchListener(new MyClickTouchListener(3,viewPager));
+    List<TextView> initTextView() {
+        TextView execute, delay, delete, finish, note;
+        execute = (TextView) getView().findViewById(R.id.normal_execute_tab_title);
+        execute.setOnTouchListener(new MyClickTouchListener(0, viewPager));
+        delay = (TextView) getView().findViewById(R.id.normal_delay_tab_title);
+        delay.setOnTouchListener(new MyClickTouchListener(1, viewPager));
+        delete = (TextView) getView().findViewById(R.id.normal_delete_tab_title);
+        delete.setOnTouchListener(new MyClickTouchListener(2, viewPager));
+        finish = (TextView) getView().findViewById(R.id.normal_finish_tab_title);
+        finish.setOnTouchListener(new MyClickTouchListener(3, viewPager));
+        note = (TextView) getView().findViewById(R.id.normal_note_tab_title);
+        note.setOnTouchListener(new MyClickTouchListener(4, viewPager));
 
-        List<TextView> textViews=new ArrayList<TextView>();
+        List<TextView> textViews = new ArrayList<TextView>();
         textViews.add(execute);
         textViews.add(delay);
         textViews.add(delete);
         textViews.add(finish);
+        textViews.add(note);
 
         return textViews;
 
@@ -139,65 +157,132 @@ public class Normal extends Fragment {
 
     /**
      * 获得屏幕的信息，其中包括有长和宽
+     *
      * @return
      */
-    DisplayMetrics getDisplayMetrice()
-    {
-        DisplayMetrics displayMetrics=new DisplayMetrics();
+    DisplayMetrics getDisplayMetrice() {
+        DisplayMetrics displayMetrics = new DisplayMetrics();
         getActivity().getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
         return displayMetrics;
     }
 
 
-    //以下代码是处理Normal_Executing
+    //以下代码是处理ViewPager内部的Activity代码
 
-    void initExecuteControls(){
-        listView=(ListView)getActivity().findViewById(R.id.normal_execute_listview);
-        itemUtil=new ItemUtil();
-        getExecuteDate();
-    }
-
+    /**
+     * 获得数据，
+     *
+     * @param type 指定获取类型
+     */
     List<Item> itemList;
-    void getExecuteDate()
-    {
+    View head;
 
-        itemList=itemUtil.getItems(StatusUtil.EXECUTE);
-        List<HashMap<String,Object>> datas=new ArrayList<HashMap<String, Object>>();
-        if(!itemList.isEmpty())
-        {
-            for(Item item:itemList)
-            {
-                HashMap<String,Object> d=new HashMap<String, Object>();
-                d.put("content",item.getContent());
+    void getDate(int type) {
+        itemList = itemUtil.getItems(type);
+        List<HashMap<String, Object>> datas = new ArrayList<HashMap<String, Object>>();
+        if (!itemList.isEmpty()) {
+            for (Item item : itemList) {
+                HashMap<String, Object> d = new HashMap<String, Object>();
+                d.put("content", item.getContent());
                 d.put("begindatetime", DateUtil.getDateString(item.getBeginDateTime()));
-                d.put("enddatetime",DateUtil.getDateString(item.getEndDateTime()));
+                d.put("enddatetime", DateUtil.getDateString(item.getEndDateTime()));
                 datas.add(d);
             }
         }
 
-        SimpleAdapter simpleAdapter=new SimpleAdapter(this.getActivity(),datas,R.layout.listview_item,new String[]{"content","begindatetime","enddatetime"},new int[]{R.id.listview_item_lv_title,R.id.listview_item_1,R.id.listview_item_2});
+
+        SimpleAdapter simpleAdapter = new SimpleAdapter(this.getActivity(), datas, R.layout.listview_item, new String[]{"content", "begindatetime", "enddatetime"}, new int[]{R.id.listview_item_lv_title, R.id.listview_item_1, R.id.listview_item_2});
         listView.setAdapter(simpleAdapter);
         listView.setOnItemClickListener(clickListener);
         listView.setOnItemLongClickListener(longClickListener);
+        listView.setOnScrollListener(scrollListener);
     }
 
-    AdapterView.OnItemClickListener clickListener=new AdapterView.OnItemClickListener() {
+    AdapterView.OnItemClickListener clickListener = new AdapterView.OnItemClickListener() {
         @Override
         public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
-            int itemId=itemList.get(position).getID();
-            Intent intent=new Intent(getActivity(),Add.class);
-            intent.putExtra("id",itemId);
+            int itemId = itemList.get(position).getID();
+            Intent intent = new Intent(getActivity(), Add.class);
+            intent.putExtra("id", itemId);
             startActivity(intent);
         }
     };
 
-    AdapterView.OnItemLongClickListener longClickListener=new AdapterView.OnItemLongClickListener() {
+    AdapterView.OnItemLongClickListener longClickListener = new AdapterView.OnItemLongClickListener() {
         @Override
         public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
             return false;
         }
     };
+
+    //是否下拉时更新数据，是否拉到最底时加载数据，是正更新完毕
+    boolean scrollTop = false, scrollbottom = false, updatefinish = true;
+
+    AbsListView.OnScrollListener scrollListener = new AbsListView.OnScrollListener() {
+        @Override
+        public void onScrollStateChanged(AbsListView view, int scrollState) {
+
+            if (scrollState == 0) {
+                if (updatefinish) {//如果更新完毕后，才可以开始下一次的更新，为了防止连续多次更新
+                    updatefinish = false;
+                    if (scrollTop) {
+                        listView.addHeaderView(head);
+                        //getDate(type);
+                        new Thread(new Runnable() {
+                            @Override
+                            public void run() {
+                                try {
+                                    Thread.sleep(500);
+                                } catch (InterruptedException e) {
+                                    e.printStackTrace();
+                                }
+                                Message msg = new Message();
+                                msg.obj = type;
+                                handler.sendMessage(msg);
+                            }
+                        }).start();
+
+                    } else if (scrollbottom) {
+
+                    }
+                }
+            }
+
+        }
+
+        Handler handler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+
+                int t = Integer.parseInt(msg.obj.toString());
+                getDate(t);
+                if (listView.getHeaderViewsCount() > 0)
+                    listView.removeHeaderView(head);
+                updatefinish = true;
+                super.handleMessage(msg);
+
+            }
+        };
+
+
+        @Override
+        public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+
+            Log.d("Normal", "firstVisibleItem is:" + firstVisibleItem + ",visibleItemCount is :" + visibleItemCount + ",totalItemCount is :" + totalItemCount);
+            if (firstVisibleItem == 0) {
+                scrollTop = true;
+                scrollbottom = false;
+            } else if (firstVisibleItem == (totalItemCount - visibleItemCount)) {
+                scrollTop = false;
+                scrollbottom = true;
+            } else {
+                scrollTop = false;
+                scrollbottom = false;
+            }
+
+        }
+    };
+
     //=========结束==========
 
 }
---
+++ src/main/java/com/example/donotforgetme/Activities/Normal_Note.java
@@ -0,0 +1,18 @@
+package com.example.donotforgetme.Activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.example.donotforgetme.R;
+
+public class Normal_Note extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_normal_note);
+    }
+
+}
